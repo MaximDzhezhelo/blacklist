4a6d044c3bd4e673c11918cb5b6ec53089951b0a
MaximDzhezhelo <mdzhezhelo@gmail.com>
2018-11-02 13:59:53 +0200

    created data module and scripts for cassandra;

---
+++ api/src/main/scala/org/test/blacklist/api/model/BlackListRule.scala
@@ -0,0 +1,3 @@
+package org.test.blacklist.api.model
+
+case class BlackListRule(msisdn: String, sn: Set[String])
---
+++ api/src/main/scala/org/test/blacklist/api/model/loader/sql/4.dataset-dict-configs.sql
@@ -0,0 +1,6 @@
+USE blacklist_dict;
+
+INSERT INTO configs JSON '{"key": "validation.msisdn.length.min", "value": "12"}';
+INSERT INTO configs JSON '{"key": "validation.msisdn.length.max", "value": "12"}';
+
+INSERT INTO configs JSON '{"key": "ndcs", "value": "67"}';
