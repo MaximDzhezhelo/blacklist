8e757103dc8b92ad0bf9dc2897a528b5604c096c
MaximDzhezhelo <mdzhezhelo@gmail.com>
2018-11-09 13:59:17 +0200

    move loader (sql) to model; created db objects;

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -1,10 +1,12 @@
 package org.test.bwl.api
 
 import akka.http.scaladsl.server.HttpApp
+import org.test.bwl.model.conf.Conf
 
-object App extends HttpApp{
+object App extends HttpApp with Conf{
 
   def main(args: Array[String]): Unit = {
+    println(conf.getList("client.db.contact.points"))
 //    startServer("0.0.0.0", 8080)
   }
 
