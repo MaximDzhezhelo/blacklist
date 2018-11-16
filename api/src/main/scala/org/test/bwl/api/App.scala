ded8213c882ee1be4c1fff3bcb286c1e6787d954
MaximDzhezhelo <mdzhezhelo@gmail.com>
2018-11-16 13:18:42 +0200

    created dao for db;

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -1,17 +1,25 @@
 package org.test.bwl.api
 
 import akka.http.scaladsl.server.HttpApp
-import org.test.bwl.model.conf.Conf
+import org.test.bwl.model.conf.DB
 
-object App extends HttpApp with Conf{
+object App extends HttpApp with DB {
 
   def main(args: Array[String]): Unit = {
-    println(conf.getList("client.db.contact.points"))
-//    startServer("0.0.0.0", 8080)
+
+    // println(conf.getList("client.db.contact.points"))
+
+    //    println(">>>" + session.getState.getConnectedHosts)
+
+    //    println(">>>" + configAccessor.getAll.all)
+    //    println(">>>" + userAccessor.getAll.all)
+
+    println(">>>" + configs)
+    println(">>>" + users)
+
+    //startServer("0.0.0.0", 8080)
   }
 
   override def routes = pathSingleSlash {
-    complete("Hello world with akka :) !")
+    complete("Hello Akka!")
   }
-
-}
