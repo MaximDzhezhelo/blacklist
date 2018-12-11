39bb7044e45a4b59d9e22cfa19a248d124d6a89c
MaximDzhezhelo <mdzhezhelo@gmail.com>
2018-12-11 13:51:58 +0200

    fixed msisdn to another type;

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -6,23 +6,13 @@ import org.test.bwl.model.conf.DB
 object App extends HttpApp with DB {
 
   def main(args: Array[String]): Unit = {
-
-//    println(">>>" + configs)
-//    println(">>>" + users)
-
-//    println(blackListRuleMapper.get("380670000001"))
-
-//    println(blackListRuleAccessor.get("380670000001").one)
-
     startServer("0.0.0.0", 8080)
   }
 
-//  override def routes = pathSingleSlash {
-//    complete("Hello Akka!")
-//  }
-
   override def routes = path("api"/"rules"/LongNumber) { msisdn =>
-    complete(s"Hello Akka! and $msisdn" )
+    val rule = blackListRuleAccessor.get(msisdn).one()
+
+    complete(s"Hello Akka! and msisdn: $msisdn and rule: $rule" )
   }
 
 }
